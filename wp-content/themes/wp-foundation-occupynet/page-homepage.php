96fa207b64b840341fa1d5d79d5451bb2845beb1
Pea <patricia@nycga.net>
2013-04-29 05:00:21 -0400

    added widgets and placed on home page

--- wp-content/themes/wp-foundation-occupynet/page-homepage.php
+++ wp-content/themes/wp-foundation-occupynet/page-homepage.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: InterOcc Homepage
+Template Name: Homepage
 */
 ?>
 
@@ -12,64 +12,77 @@ Template Name: InterOcc Homepage
           
           <article role="article">
           
-            <?php
-
-            $orbit_slider = of_get_option('orbit_slider');
-            if ($orbit_slider){
-
-            ?>
             
-            <header>
-            
-              <div id="featured">
-
-                <?php
-                  global $post;
-                  $tmp_post = $post;
-                  $args = array( 'numberposts' => 5 );
-                  $myposts = get_posts( $args );
-                  foreach( $myposts as $post ) :  setup_postdata($post); 
-                    $post_thumbnail_id = get_post_thumbnail_id();
-                    $featured_src = wp_get_attachment_image_src( $post_thumbnail_id, 'wpf-home-featured' );
-                ?>
-                
-                <div style="background-color: #F2F2F2;">
-                  <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                  <?php the_excerpt(); ?>
-                  <p><a href="<?php the_permalink(); ?>" class="button nice radius">Read more »</a></p>
-                </div>
-                
-                <?php endforeach; ?>
-                <?php $post = $tmp_post; ?>
-
-              </div>
-              
-            </header>
-
-            <script type="text/javascript">
-               $(window).load(function() {
-                   $('#featured').orbit({ 
-                    fluid: '16x3'
-                   });
-               });
-            </script>
-
-            <?php } ?>
-
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
             <section class="row post_content">
 
-              <?php get_sidebar(); // sidebar 1 ?>
+              <?php get_sidebar('sidebarleft'); // sidebar left ?>
 
             
               <div class="home-main six columns">
+
+              <!-- Homepage tagline -->
+
+              <?php echo get_post_meta($post->ID, 'custom_tagline', true); ?>
+
+              <!-- Slider -->
+
+              <?php
+
+              $orbit_slider = of_get_option('orbit_slider');
+              if ($orbit_slider){
+
+              ?>
+              
+              <header>
+              
+                <div id="featured">
+
+                  <?php
+                    global $post;
+                    $tmp_post = $post;
+                    $args = array( 'numberposts' => 5 );
+                    $myposts = get_posts( $args );
+                    foreach( $myposts as $post ) :  setup_postdata($post); 
+                      $post_thumbnail_id = get_post_thumbnail_id();
+                      $featured_src = wp_get_attachment_image_src( $post_thumbnail_id, 'wpf-home-featured' );
+                  ?>
+                  
+                  <div style="background-color: #F2F2F2;">
+                    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                    <?php the_excerpt(); ?>
+                    <p><a href="<?php the_permalink(); ?>" class="button nice radius">Read more »</a></p>
+                  </div>
+                  
+                  <?php endforeach; ?>
+                  <?php $post = $tmp_post; ?>
+
+                </div>
+                
+              </header>
+
+              <script type="text/javascript">
+                 $(window).load(function() {
+                     $('#featured').orbit({ 
+                      fluid: '16x7'
+                     });
+                 });
+              </script>
+
+              <?php } ?>
+
+              <!-- //slider -->
+
+               <?php wp_reset_query(); ?> 
             
-                <?php the_content(); ?>
+              <!-- Main Content -->
+
+              <?php the_content(); ?>
                 
               </div>
               
-              <?php get_sidebar('sidebar2'); // sidebar 2 ?>
+              <?php get_sidebar('sidebarright'); // sidebar right ?>
                           
             </section> <!-- end article header -->
             
@@ -104,25 +117,7 @@ Template Name: InterOcc Homepage
           
           <?php endif; ?>
 
-          <!-- Call to Action Panel -->
-          <div class="row">
-            <div class="twelve columns">
-            
-              <div class="panel">
-                <h4>Get in touch!</h4>
-                    
-                <div class="row">
-                  <div class="nine columns">
-                    <p>We'd love to hear from you, you attractive person you.</p>
-                  </div>
-                  <div class="three columns">
-                    <a href="#" class="radius button right">Contact Us</a>
-                  </div>
-                </div>
-              </div>
-              
-            </div>
-          </div>
+          <?php get_sidebar('actionpanel'); // actionpanel ?>
       
         </div> <!-- end #main -->
         
