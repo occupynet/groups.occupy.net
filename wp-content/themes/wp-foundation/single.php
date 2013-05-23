5c53e2157237fc6f165a4de0ef77093b2b7e6d03
Erica <e@energy7.com>
2013-05-22 23:27:21 -0400

    remove "posted by" and ampersand from meta tags, replace comma with bar
    Signed-off-by: Erica <e@energy7.com>

--- wp-content/themes/wp-foundation/single.php
+++ wp-content/themes/wp-foundation/single.php
@@ -14,7 +14,7 @@
 							
 							<h1 class="single-title" itemprop="headline"><?php the_title(); ?></h1>
 							
-							<p class="meta"><?php _e("Posted", "bonestheme"); ?> <time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time('F jS, Y'); ?></time> <?php _e("by", "bonestheme"); ?> <?php the_author_posts_link(); ?> <span class="amp">&</span> <?php _e("filed under", "bonestheme"); ?> <?php the_category(', '); ?>.</p>
+							<p class="meta"><time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time('F jS, Y'); ?></time> <?php _e("by", "bonestheme"); ?> <?php the_author_posts_link(); ?> | <?php the_category(' | '); ?></p>
 						
 						</header> <!-- end article header -->
 					
