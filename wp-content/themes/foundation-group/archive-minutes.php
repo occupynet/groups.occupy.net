9151b0c651fdcb71ae3abbfd2668f6bff2f54fbb
Pea <patricia@nycga.net>
2013-05-24 19:00:39 -0400

    added template for Minutes display

--- wp-content/themes/foundation-group/archive-minutes.php
+++ wp-content/themes/foundation-group/archive-minutes.php
@@ -1,50 +1,49 @@
 <?php get_header(); ?>
 			
-			<div id="content">
+			<div id="content" class="clearfix">
 			
 				<div id="main" class="eight columns clearfix" role="main">
-
+						<h1 class="archive_title h2">
+							Minutes
+						</h1>
+					
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					
 					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
 						
+						<?php $meeting_date = get_post_meta($post->ID,'be_meeting_date',true); ?>
 						<header>
 							
-							<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-													
+							<h3 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?> <?php echo $meeting_date; ?></a></h3>
+							
+							<p class="meta">
+								<?php _e("Posted", "bonestheme"); ?> <time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time('F jS, Y'); ?></time> <?php _e("by", "bonestheme"); ?> <?php the_author_posts_link(); ?>.</p>
+						
 						</header> <!-- end article header -->
 					
-						<section class="post_content clearfix">
-							<div class="post-thumbnail">
-							<a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail( 'wpf-featured' ); ?></a>
-							</div>
-
-							<?php the_excerpt('Read more &raquo;'); ?>
+						<section class="post_content">
+						
+						
+							<?php the_excerpt(); ?>
 					
 						</section> <!-- end article section -->
 						
 						<footer>
-			
-							<h6 class="meta"><time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time('F jS, Y'); ?></time> <?php _e("by ", "bonestheme"); ?><?php the_author_posts_link(); ?> | <?php the_category(' | '); ?>.</h6>
-							<h6 class="tags"><?php the_tags('<span class="tags-title">Tags:</span> ', ' ', ''); ?></h6>
+	
+							<p class="tags"><?php the_tags('<span class="tags-title">Tags:</span> ', ' ', ''); ?></p>
 
-							<?php edit_post_link('edit', '<p>', '</p>'); ?>
-
-							<div style="clear:both;">
-							</div>
+							<div style="clear:both;"></div>
 							
 						</footer> <!-- end article footer -->
 					
 					</article> <!-- end article -->
-					
-					<?php comments_template(); ?>
 					
 					<?php endwhile; ?>	
 					
 					<?php if (function_exists('page_navi')) { // if expirimental feature is active ?>
 						
 						<?php page_navi(); // use the page navi function ?>
-						
+
 					<?php } else { // if it is disabled, display regular wp prev & next links ?>
 						<nav class="wp-prev-next">
 							<ul class="clearfix">
@@ -52,16 +51,17 @@
 								<li class="next-link"><?php previous_posts_link(_e('Newer Entries &raquo;', "bonestheme")) ?></li>
 							</ul>
 						</nav>
-					<?php } ?>		
+					<?php } ?>
+								
 					
 					<?php else : ?>
 					
 					<article id="post-not-found">
 					    <header>
-					    	<h1>Not Found</h1>
+					    	<h1><?php _e("No Posts Yet", "bonestheme"); ?></h1>
 					    </header>
 					    <section class="post_content">
-					    	<p>Sorry, but the requested resource was not found on this site.</p>
+					    	<p><?php _e("Sorry, What you were looking for is not here.", "bonestheme"); ?></p>
 					    </section>
 					    <footer>
 					    </footer>
--- wp-content/themes/foundation-group/functions.php
+++ wp-content/themes/foundation-group/functions.php
@@ -113,4 +113,15 @@ function show_template() {
     }
 }
 
+/************* ADD POSTS 2 POSTS CONNECTION BOX ********************/
+
+function my_connection_types() {
+    p2p_register_connection_type( array(
+        'name' => 'posts_to_pages',
+        'from' => 'post',
+        'to' => 'page'
+    ) );
+}
+add_action( 'p2p_init', 'my_connection_types' );
+
 ?>
