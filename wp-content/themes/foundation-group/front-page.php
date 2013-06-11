5ac11dfb83cb9e890becd7d83471f38a610850c7
Pea <patricia@nycga.net>
2013-06-10 21:08:44 -0400

    moved metadata into footer in front-page.php

--- wp-content/themes/foundation-group/front-page.php
+++ wp-content/themes/foundation-group/front-page.php
@@ -103,6 +103,8 @@ Template Name: Homepage
 				<header>
 					
 					<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+
+					<h6 class="meta"><time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time('F jS, Y'); ?></time> <?php _e("by ", "bonestheme"); ?><?php the_author_posts_link(); ?> | <?php the_category(' | '); ?></h6>
 									
 				</header> <!-- end article header -->
 			
@@ -117,7 +119,6 @@ Template Name: Homepage
 				
 				<footer>
 	
-					<h6 class="meta"><time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time('F jS, Y'); ?></time> <?php _e("by ", "bonestheme"); ?><?php the_author_posts_link(); ?> | <?php the_category(' | '); ?></h6>
 					<h6 class="tags"><?php the_tags('<span class="tags-title"></span> ', ' ', ''); ?></h6>
 
 					<div style="clear: both;"></div>
